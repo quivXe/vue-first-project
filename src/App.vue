b69a1dbe60aa7c8a5851c293c0178d17b7a1da87
Szymon <sgrabowki789@gmail.com>
2024-09-30 01:26:13 +0200

    added back button and few bug fixes with new tasks

--- src/App.vue
+++ src/App.vue
@@ -3,40 +3,68 @@ import { ref, reactive, computed  } from 'vue'
 import TodoHeader from './components/todoHeader/todoHeader.vue'
 import TodoItem from "./components/todoItem/todoItem.vue"
 import TodoAddItem from "./components/todoItem/todoItemAdd.vue"
+import TodoBackButton from "./components/todoBackButton.vue"
 
-const title = ref("changed title")
 var id = 0;
-// const tasks = ref([
-//   {id: id++, "name": `do task n.${id}`},
-//   {id: id++, "name": `do task n.${id}`},
-//   {id: id++, "name": `do task n.${id}`},
-// ])
 const allTasks = ref([
   {
     id: id++,
     "name": `do task n.${id}`,
     subTasks: [
-      {id: id++, "name": `1do task n.${id}`},
-      {id: id++, "name": `1do task n.${id}`},
-      {id: id++, "name": `1do task n.${id}`},
+      {
+        id: id++,
+        "name": `1do task n.${id}`,
+        subTasks: []
+      },
+      {
+        id: id++, 
+        "name": `1do task n.${id}`,
+        subTasks: []
+      },
+      {
+        id: id++, 
+        "name": `1do task n.${id}`,
+        subTasks: []
+      },
     ]
   },
   {
     id: id++,
     "name": `do task n.${id}`,
     subTasks: [
-      {id: id++, "name": `2do task n.${id}`},
-      {id: id++, "name": `2do task n.${id}`},
-      {id: id++, "name": `2do task n.${id}`},
+      {
+        id: id++, 
+        "name": `2do task n.${id}`,
+        subTasks: []
+      },
+      {
+        id: id++, 
+        "name": `2do task n.${id}`,
+        subTasks: []
+      },
+      {
+        id: id++, 
+        "name": `2do task n.${id}`,
+        subTasks: []
+      },
     ]
   },
   {
     id: id++,
     "name": `do task n.${id}`,
     subTasks: [
-      {id: id++, "name": `3do task n.${id}`},
-      {id: id++, "name": `3do task n.${id}`},
-      {id: id++, "name": `3do task n.${id}`},
+      {
+        id: id++, 
+        "name": `3do task n.${id}`
+      },
+      {
+        id: id++, 
+        "name": `3do task n.${id}`
+      },
+      {
+        id: id++, 
+        "name": `3do task n.${id}`
+      },
     ]
   },
 ])
@@ -44,9 +72,14 @@ const allTasks = ref([
 const currentTasks = computed(() => {
   return getCurrentTasks()
 })
+const title = computed(() => {
+  return parentTree.value.length?
+    parentTree.value[parentTree.value.length - 1].name :
+    "VUE TODO APP"
+})
 function getCurrentTasks(index = 0, subTasks = allTasks.value) {
   if (parentTree.value[index] === undefined) return subTasks;
-  let currentTask = subTasks.find(task => task.id === parentTree.value[index]);
+  let currentTask = subTasks.find(task => task.id === parentTree.value[index].id);
 
   if (!currentTask || !currentTask.subTasks) return null; // Return null if something goes wrong (e.g., task not found)
 
@@ -54,15 +87,13 @@ function getCurrentTasks(index = 0, subTasks = allTasks.value) {
 }
 
 function onTaskPressed(task) {
-  parentTree.value.push(task.id);
-  title.value = task.name;
+  parentTree.value.push(task);
 }
 
 function onDeleteTask(task) {
   let _currentTasks = getCurrentTasks();
   const index = _currentTasks.findIndex(t => t.id === task.id);
   if (index !== -1) _currentTasks.splice(index, 1);
-  console.log(allTasks.value)
 }
 function onNewTaskBlur(value) {
   if (value === "") return;
@@ -71,14 +102,18 @@ function onNewTaskBlur(value) {
 function addTask(value) {
   getCurrentTasks().push({
     id: id++,
-    name: value
+    name: value,
+    subTasks: []
   });
 }
-
+function popParent() {
+  parentTree.value.pop();
+}
 const parentTree = ref([]);
 </script>
 
 <template>
+<TodoBackButton @backPressed="popParent"></TodoBackButton>
 <TodoHeader :title="title"/>
 <ul>
   <TodoItem v-for="task in currentTasks" :task="task" @task-pressed="onTaskPressed" @delete-task="onDeleteTask"/>
